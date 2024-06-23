1eac70ac1ac78b3c881dbead6ca59705a776bf74
marwnsamy <110399811+marwnsamy@users.noreply.github.com>
2024-06-23 14:17:23 +0300

    review codes

---
+++ CODES FILE/CMDER-order.cmd
@@ -0,0 +1,74 @@
+@REM :mkdir (name)  :تفتح  ملف  جديد
+@REM :git clone (link  repo  github) :  اربط  ملف بمنطقه  التخزين  في  جيت  هب
+@REM :cd  : علشان   افتح   الملف او  احدد  الملف   اللي هشتغل   فيه
+@REM :git status  :   علشان   اعرف  حاله  الشغل  
+@REM :git add  (name of  file)  :  اضافه  ملف  علشان  تستعد  للرفع  على  الريبو
+@REM :git add *  :  اضافه  ملفات  بدون   تحديد  علشان  تستعد  للرفع  على  الريبو
+@REM :git reset  hed  : علشان   اشيل   الملفات  من  قايمه   الرفع
+@REM :git commit  -m""  :ضروري  قبل  الرفع  يتعمل  قبل  الرفع  مش   هيترفع  بدونه  اعمل  تعليق  
+@REM :git branch  :  عرض  قائمه   الفروع  
+@REM :git remote -v  :  عرض  ق ائمه الريموت   نيم
+@REM :git puch remote  branch : علشان  يترفع  الملفات   على   الجيت  هب
+@REM :git pull  :  لسحب  التعديلات  من  remote repo
+@REM :git config  -l  --system   :  عرض إعدادات Git على مستوى النظام
+@REM :git config  -l  --local    :  عرض إعدادات Git على مستوى المستودع
+@REM :git config  -l --global   :  عرض إعدادات Git على مستوى المستخدم
+@REM :git config  -l    :   علشان اعرض اعدادات  git
+@REM :touch  :انشاء  ملف  جديد  
+@REM :ssh-kygen  -t rsa  -b  4096  "mail"  :  اعمل  باسورد  للملفات
+@REM :cat  :  اعرف  ايه  اللي  في   الملف  and  can  use  to  copy   by  >
+
+@REM :echo "":print  text   in  anything  (>use  to  write  inside  file)(>>use   to add  mpre  text  inside  file)
+@REM :ssh -t git@github.com  :  تجربه   للباسورد
+@REM :git init  :  علشان   اعمل   new  repo
+@REM :git config --global alias -  :  علشان  اختصار  لأمر
+@REM :git branch (name)  :  علشان   اعمل  فرع  جديد
+@REM :git checkout (name)  :اغير  لفرع   اخر
+@REM :git branch -d  :  احف  الفرع  ويشترط   ان  يكون  فارغ
+@REM :git branch -D  :  احذف فرع   بالقوه
+@REM :git branch -m  :تعديل   اسم   الفرغ
+@REM :git merge (name)  : دمج   اكتر  من  فرع
+@REM :git checkout -b (name)    :  اعمل  فرع   جديد  وانتقل   ليه
+@REM :git stach  :علشان   اركن    ملف  على  جنب 
+@REM :git stach  pop  :  اطلعه  من  الركنه
+@REM :git stach list  :  عرض قائمة بجميع التغييرات المخزنة
+@REM :git stach apply  :لتطبيق تغييرات من فرع آخر على فرعك الحالي او تغييرات من مستودع آخر على مستودع او  تطبيق  تغيير من  اصدار  قديم  او  تطبيق  تغير  اتبعت  بالايميل  او ما ي شابه
+@REM :git stach pop  stach@{}  :  استعاده  تغييرات  من stach  اخر  
+@REM :git stach drop  :  اجذف   اي  صندوق
+@REM :git stach show  :يبين ال  stach    جواه   ايه
+@REM :git stach clear  : حذف كب  الملفات  في  stach
+@REM :git restore  --staged (name)  : إزالة ملف من منطقة المؤقتة (staging area) دون التأثير على محتوى الملف الفعلي في مساحة العمل.
+@REM :git clean -n  :إزالة الملفات غير المُتَتَبَّعة من مسار العمل الخاص بك 
+@REM :git clean -f  :  أمرًا قويًا في Git يُستخدم لإزالة الملفات غير المُتتبعة من مستودع Git
+@REM :git log  : علشان  يعرض   اخر تعليق
+@REM :git reset --hard #  :  علشان يغير  التعليق  لواحد  تاني
+@REM :cod-gitignore  :  يتمم  التعديل  على   Vcode
+@REM :git add-f (file name)  :اضافه  ملف  بغض  النظ  عن   انو  محجوب   ام   لا
+@REM :rmdir : deletes empty folders.
+@REM :rm -r :forcefully removes a folder
+@REM :history  -c:remove  all  orders
+@REM :move or  mv  :  move file  to anouther
+@REM :علشان   ارجع  ملف  من  جوا  لبرا  استخدم  ..\
+@REM :rename x y:change  name  from  x  to  y
+@REM :cp:copy  if the  file  is empty
+@REM :cp  -r  x y:copy  files  forsetly from  x  to  y
+@REM :grep  :  search  ----"keyword"  file  name     (-r=at  all)
+@REM :file :to  know the  kind  of   file
+@REM :/?:  to  know  all   manual  about  order
+@REM :ctrl+c: stop  process
+@REM :tasklist   taskkill  :to  know  process  working  and kill  it  be  (/PID)(/IM)
+@REM :explorer  :  show  file
+@REM :alias  f=file  name:  shortcut  order   
+@REM :&&:  to  do  2order
+@REM :whoami: generate  name
+@REM :systeminfo:  information  system
+@REM :ipconfig:  information  about  ip
+@REM :| clip:  copy  text  or  anything  to clipbourd
+@REM :code  :open  Vcode  from  cmd
+https://ss64.com/  =  uses to  search  and  review   all  orders  
+
+funny order cms=d:
+                            Dancing Parrot - curl ascii.live/parrot
+                            Colorful Dancing Parrot - curl parrot.live
+                            Running Man - curl ascii.live/forrest
+                            Rick Roll - curl ascii.live/can-you-hear-me
---
+++ CODES FILE/CPP/c++ codes.cpp
@@ -0,0 +1,44 @@
+// 1-cout->text ti displayed on the screen
+//                                            ============================================
+// 2 - cin->variables data taypes
+//                                            ============================================
+// 3-const->قيمه ثابته مش هينفع تتعدل
+//                                            ============================================
+// 4-
+// control statment:
+//                          if cases:
+/*
+    a- if (expression)
+        action statment
+
+b- if (expression)
+        action statment
+else
+        action statment
+
+c- if (expression)
+        action statment
+else if (expression)
+        action
+else
+        action
+*/
+
+//                          switch statment
+/* switch (expression{
+        case constant 1:
+            action statment;
+            break;
+        case constant 2:
+            action statment;
+            break;
+        case constant 3:
+            action statment;
+            break;
+        defult:
+            action statment;
+})
+*/
+// must be #int#char#
+// be only specific value only
+//                                             ============================================
